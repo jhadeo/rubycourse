07d55e637f80d7a411e16a12d71e1c7c3d22710c
jhadeo <jhade.orbigoso@gmail.com>
2022-06-02 10:47:02 +0800

    added a file

---
+++ Array.rb
@@ -0,0 +1,20 @@
+array = ["Ruby", "test", 400, "John"]
+p array[2]
+
+#shovel
+shovel_array = array << "Hey!"
+
+p shovel_array
+
+#deleting arrays
+
+# delete
+# delete_at
+# delete_if
+
+delete_array= [123, "Hi!", "Hello!"]
+
+mod_array = delete_array.delete(123)
+p delete_array
+
+#nested array
