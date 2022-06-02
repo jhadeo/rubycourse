7238b3e1b88cada87ae860adba58d6a0d6e198c2
jhadeo <105715821+jhadeo@users.noreply.github.com>
2022-06-02 19:33:12 +0800

    Update If_else.rb

--- If_else.rb
+++ If_else.rb
@@ -1,14 +1,13 @@
 # If else review app
 # make a password "guarded" app with if else statements
-
+  PASSWORD = "banana"
 def user_input
   puts "Please enter your name."
     username = gets.chomp
 
   puts "Hello #{username}! please enter your password"
   user_pass = gets.chomp
-  password = "banana"
-  if user_pass == password
+  if user_pass == PASSWORD
     print "Welcome! there is nothing here."
   else
     print "Wrong or no password! try again"
